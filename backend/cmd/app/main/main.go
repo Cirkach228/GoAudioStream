24d4327561b3f4bb9397fe9608f6bd26c273eeca
Cirkach228 <svi082007@gmail.com>
2023-12-26 19:48:00 +0300

    1

--- backend/cmd/app/main/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/Cirkach228/GoAudioStream/backend/internal/transport/rest/handler"
-
-func main() {
-	Hi()
-}
---
+++ backend/server/cmd/app/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Cirkach228/GoAudioStream/backend/server/internal/transport/rest/handler"
+)
+
+func main() {
+	handler.Hi()
+}
