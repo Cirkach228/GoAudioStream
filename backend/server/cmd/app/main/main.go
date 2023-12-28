555643efabc11643ea88dc2c1a0c6522ac74e93b
Cirkach228 <svi082007@gmail.com>
2023-12-28 12:00:49 +0300

    testing main page handler

--- backend/server/cmd/app/main/main.go
+++ backend/server/cmd/app/main/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	handler.Hi()
+	handler.runHandlers()
 }
--- backend/server/internal/transport/rest/handler/handler.go
+++ backend/server/internal/transport/rest/handler/handler.go
@@ -1,6 +1,13 @@
 package handler
 
-import "fmt"
-func Hi() {
-	fmt.Println("Hi")
+import ("fmt"
+		"net/http"
+)
+func mainPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func runHandlers(){
+	http.HandleFunc("/",mainPageHandler)
+	http.ListenAndServe(":8080",nil)
 }
